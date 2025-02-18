c1f619cef955bf76f0535384297b2de7f936f854
Rtollinchi <rtollinchi23@gmail.com>
2025-02-18 16:58:57 -0500

    updated PokemonList.jsx to render Pokemon images

--- src/PokemonList.jsx
+++ src/PokemonList.jsx
@@ -23,14 +23,17 @@ const PokemonList = () => {
     <div className="p-4">
       <h1 className="text-3xl font-bold text-center mb-4">Pokédex</h1>
       <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-6 gap-4">
-        {pokemon.map((poke, index) => (
-          <div
-            key={index}
-            className="p-4 bg-gray-100 rounded shadow text-center capitalize"
-          >
-            <p className="text-lg font-medium">{poke.name}</p>
-          </div>
-        ))}
+        {pokemon.map((poke, index) => {
+          const pokeId = index + 1; // Pokémon IDs are sequential (1-151)
+          const imageUrl = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${pokeId}.png`;
+
+          return (
+            <div key={pokeId} className="p-4 bg-gray-100 rounded shadow text-center capitalize">
+              <img src={imageUrl} alt={poke.name} className="w-20 h-20 mx-auto" />
+              <p className="text-lg font-medium">{poke.name}</p>
+            </div>
+          );
+        })}
       </div>
     </div>
   );
